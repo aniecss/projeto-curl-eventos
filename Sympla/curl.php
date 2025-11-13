99e0cbe143dc9583eab260d64baea43d08c0d781
aniecss <kariohana10@gmail.com>
2025-11-12 22:14:55 -0300

    Update curl.php

--- Sympla/curl.php
+++ Sympla/curl.php
@@ -4,21 +4,18 @@ use Dom\XPath;
 
 $endpoint = 'https://www.sympla.com.br/eventos?s=santos';
 
-$cRUL = curl_init(); // Variável inicializada
+$cRUL = curl_init(); 
 
 curl_setopt($cRUL, CURLOPT_URL, $endpoint);
 
 curl_setopt($cRUL, CURLOPT_RETURNTRANSFER, true);
 
-// CORREÇÃO: Usar $cRUL (a variável correta) em vez de $scRUL
 $response = curl_exec($cRUL); 
 
 if (curl_errno($cRUL)) {
-    echo "Erro cRUL: " . curl_error($cRUL); // Melhorando a mensagem de erro
+    echo "Erro cRUL: " . curl_error($cRUL); 
 } else {
     print_r($response);
 }
 
 curl_close($cRUL);
-
-// & "C:\xampp\php\php.exe" C:\xampp\htdocs\Projeto_Curl\Sympla/curl.php
